52ea1508ce86518f40b619570bfbcb5303d1760b
Jaison Robson Gusava <jaison.gusava@gett.com.br>
2018-09-18 16:28:00 -0300

    teste1

--- graphql-tutorial/app/graphql/resolvers/create_vote.rb
+++ graphql-tutorial/app/graphql/resolvers/create_vote.rb
@@ -4,9 +4,6 @@ class Resolvers::CreateVote < GraphQL::Function
     type Types::VoteType
   
     def call(_obj, args, ctx)
-        # puts "Argumentos:", args.to_json
-        # puts args[:linkId]
-        puts Link.find_by(id: args[:linkId])
       Vote.create!(
         link: Link.find_by(id: args[:linkId]),
         user: ctx[:current_user]
